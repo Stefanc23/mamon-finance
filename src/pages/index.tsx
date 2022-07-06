dd9d8a5ccb34230f68d0e9e9528aa7e7a2949f29
Stefano Christian Wiryana <stefano.wiryana@binus.ac.id>
2022-07-06 19:12:52 +0700

    fix: landing page not loading correctly

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,9 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useSession } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
 
-const Home: NextPage = () => {
-  const { data: session } = useSession();
-  const greetings = ['Hello', 'Bonjour', 'Hola'];
-
+const Home: NextPage = ({ greeting, link, linkText }: any) => {
   return (
     <div className='flex min-h-screen flex-col items-center justify-center py-2'>
       <Head>
@@ -16,26 +13,12 @@ const Home: NextPage = () => {
 
       <main className='flex w-full h-full flex-1 flex-col items-center justify-center px-20 text-center'>
         <Image src='/logo.png' alt='Mamon Finance' width={128} height={128} />
-        <h1 className='mb-6 text-3xl font-bold text-white'>
-          {session
-            ? `${greetings[Math.floor(Math.random() * greetings.length)]} ${
-                session.user?.name?.split(' ')[0]
-              }!`
-            : 'Start Managing Your Finances Now!'}
-        </h1>
-        {session ? (
-          <Link href='/dashboard'>
-            <a className='cursor-pointer flex px-5 py-3 font-medium bg-primary text-sm rounded hover:shadow-md hover:shadow-secondary focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out'>
-              Go to Dashboard
-            </a>
-          </Link>
-        ) : (
-          <Link href='/auth/signin'>
-            <a className='cursor-pointer flex px-5 py-3 font-medium bg-primary text-sm rounded hover:shadow-md hover:shadow-secondary focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out'>
-              Get Started
-            </a>
-          </Link>
-        )}
+        <h1 className='mb-6 text-3xl font-bold text-white'>{greeting}</h1>
+        <Link href={link}>
+          <a className='cursor-pointer flex px-5 py-3 font-medium bg-primary text-sm rounded hover:shadow-md hover:shadow-secondary focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out'>
+            {linkText}
+          </a>
+        </Link>
       </main>
 
       <footer className='flex w-full h-16 items-center justify-center'>
@@ -52,5 +35,31 @@ const Home: NextPage = () => {
     </div>
   );
 };
+
+export async function getServerSideProps(context: any) {
+  const session = await getSession(context);
+  const greetings = [
+    'Hello',
+    'Bonjour',
+    'Hola',
+    'Ciao',
+    'こんにちは',
+    '안녕하세요',
+    '你好',
+  ];
+
+  return {
+    props: {
+      session,
+      greeting: session
+        ? `${greetings[Math.floor(Math.random() * greetings.length)]} ${
+            session.user?.name?.split(' ')[0]
+          }!`
+        : 'Start Managing Your Finances Now!',
+      link: session ? '/dashboard' : '/auth/signin',
+      linkText: session ? 'Go to Dashboard' : 'Get Started',
+    },
+  };
+}
 
 export default Home;
